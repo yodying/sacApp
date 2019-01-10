d12adc6dfe2e4864a5ae552a0015ebcee2b7fbf1
@FadiDev <@developerfadi@outlook.com>
2019-01-10 10:42:12 +0700

    Added new title props

--- App.js
+++ App.js
@@ -3,6 +3,6 @@ import React, { Component } from "react";
 import HomeScreen from "./src/page/Home/index.js"
 export default class App extends Component {
   render() {
-    return <HomeScreen />;
+    return <HomeScreen title="ok"/>;
   }
 }
